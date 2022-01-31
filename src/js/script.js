03cff164816d8a338c965fc9a69695a8b890b989
Yago Russo <yagrrusso@gmail.com>
2022-01-30 21:57:43 -0300

    menu hamburguer

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,29 @@
-function createExp(){
-  let experience = ''
-  experiencies.forEach((exp)=>{
-    experience += `
-      <div class="experience">
-        <div class="experience-position">${exp.name}</div>
-        <div class="experience-company">${exp.company}</div>
-        <div class="experience-date">
-          <span>${exp.date.start}</span> - <span>${exp.date.end}</span>
-        </div>
-        <div class="experience-description">${exp.description}</div>
-      </div>
-    `
-  })
-  document.querySelector('.experiencies').innerHTML = experience
-}
+// function createExp(){
+//   let experience = ''
+//   experiencies.forEach((exp)=>{
+//     experience += `
+//       <div class="experience">
+//         <div class="experience-position">${exp.name}</div>
+//         <div class="experience-company">${exp.company}</div>
+//         <div class="experience-date">
+//           <span>${exp.date.start}</span> - <span>${exp.date.end}</span>
+//         </div>
+//         <div class="experience-description">${exp.description}</div>
+//       </div>
+//     `
+//   })
+//   document.querySelector('.experiencies').innerHTML = experience
+// }
+
+const menuOpener = document.querySelector('.menu-opener')
+let menuOpen = false
+
+menuOpener.addEventListener('click', ()=>{
+  if (!menuOpen){
+    menuOpener.classList.add('open')
+    menuOpen = true
+  } else {
+    menuOpener.classList.remove('open')
+    menuOpen = false
+  }
+})
