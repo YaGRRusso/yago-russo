46fca60006b66b7eeb2c937b3618d63031ec7e3f
Yago Russo <yagrrusso@gmail.com>
2022-02-04 18:16:49 -0300

    tela inicial

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,3 @@
-// function createExp(){
-//   let experience = ''
-//   experiencies.forEach((exp)=>{
-//     experience += `
-//       <div class="experience">
-//         <div class="experience-position">${exp.name}</div>
-//         <div class="experience-company">${exp.company}</div>
-//         <div class="experience-date">
-//           <span>${exp.date.start}</span> - <span>${exp.date.end}</span>
-//         </div>
-//         <div class="experience-description">${exp.description}</div>
-//       </div>
-//     `
-//   })
-//   document.querySelector('.experiencies').innerHTML = experience
-// }
-
 const menuOpener = document.querySelector('.menu-opener')
 let menuOpen = false
 
